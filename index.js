aa0b13e442965929e0e88387a0cc482a4de4d29a
Anurag Rana <anuragrana88@gmail.com>
2020-08-19 12:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var expressExtension = require('express-integrator-extension');
 var functions = require('./functions');
 
-var systemToken = '20eaaa8bb8004a29bc94ebb335ceee72'; // Set this value to the systemToken of the stack created in integrator.io
+var systemToken = 'XXXX'; // Set this value to the systemToken of the stack created in integrator.io
 var options = {
     connectors: {
         '5ca2e8a82c14ff0838165e1e': functions
